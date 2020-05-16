10096d2ad491c67bfc7f8677fb8c9660840b6649
Reinholds Zviedris <reinholds@zviedris.lv>
2020-05-17 02:15:25 +0300

    removed next/prev page from meta schema

--- hdns/schema/meta.go
+++ hdns/schema/meta.go
@@ -10,8 +10,6 @@ type Meta struct {
 type MetaPagination struct {
 	Page         int `json:"page"`
 	PerPage      int `json:"per_page"`
-	PreviousPage int `json:"previous_page"`
-	NextPage     int `json:"next_page"`
 	LastPage     int `json:"last_page"`
 	TotalEntries int `json:"total_entries"`
 }
