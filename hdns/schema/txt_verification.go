e020ff7de2094d6ef114f39edbcc20d26ebb0263
Reinholds Zviedris <reinholds@zviedris.lv>
2020-05-17 01:48:57 +0300

    fixed txt_verification linting issues

--- hdns/schema/txt_verification.go
+++ hdns/schema/txt_verification.go
@@ -1,11 +1,13 @@
 package schema
 
-// TXT verification defines the schema of TXT Verification object.
+// TxtVerification defines the schema of TXT Verification object.
 type TxtVerification struct {
 	Name  string `json:"name"`
 	Token string `json:"token"`
 }
 
+// TxtVerificationResponse defines the schema of the response when
+// retrieving TXT Verification object.
 type TxtVerificationResponse struct {
 	TxtVerification TxtVerification `json:"txt_verification"`
 }
